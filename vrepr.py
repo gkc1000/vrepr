bc51d122f8c00e3d578e9abe5a23849d2bdfaeed
gkc1000 <gkc1000@gmail.com>
2021-09-05 21:07:40 -0700

    Add files via upload

--- vrepr.py
+++ vrepr.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import numpy as np
+import scipy
 from scipy import linalg as la
 from scipy import optimize as opt
 
@@ -20,22 +21,6 @@ def gen_rho(n, theta):
                   [np.sin(theta), np.cos(theta)]])
     return np.dot(u * occ, u.T)
 
-def test_gen_rho():
-    n = 0.5
-    theta = 0.0
-    assert la.norm(gen_rho(n, theta) - np.diag([0.5, 0.5])) < 1e-12
-    
-    n = 0.1
-    theta = 0.0
-    assert la.norm(gen_rho(n, theta) - np.diag([0.1, 0.9])) < 1e-12
-    
-    n = 0.1
-    theta = np.pi
-    assert la.norm(gen_rho(n, theta) - np.diag([0.1, 0.9])) < 1e-12
-    
-    n = 0.1
-    theta = np.pi * 0.5
-    assert la.norm(gen_rho(n, theta) - np.diag([0.9, 0.1])) < 1e-12
 
 def tb(L, pbc=False):
     """
@@ -50,36 +35,6 @@ def tb(L, pbc=False):
         h[-1, 0] = 1.0
     return h
 
-def test_tb():
-    h_tb = tb(6)
-    assert np.allclose(h_tb.diagonal(offset=1) , 1)
-    assert np.allclose(h_tb.diagonal(offset=-1), 1)
-    
-    h_tb = tb(6, pbc=True)
-    assert np.allclose(h_tb.diagonal() , 0)
-    assert np.allclose(h_tb.diagonal(offset=1) , 1)
-    assert np.allclose(h_tb.diagonal(offset=-1), 1)
-    assert h_tb[0, 5] == 1.0
-    assert h_tb[5, 0] == 1.0
-    
-def unitdm4(u, full=False):
-    h = tb(4)
-    nao = 2
-    nocc = 2
-    h[0, 1] += u[0]
-    h[1, 0] += u[0]
-    h[1, 1] += u[1]
-
-    h[2, 3] += u[0]
-    h[3, 2] += u[0]
-    h[2, 2] += u[1]
-    
-    e, v = np.linalg.eigh(h)
-    dm = np.dot(v[:nocc], v[:nocc].conj().T)
-    if full:
-        return dm
-    else:
-        return dm[:nao, :nao]
 
 def add_u_pbc(h, u):
     L = h.shape[0]
@@ -103,34 +58,10 @@ def dm_pbc(u, L, occ=None):
     return dm
 
     
-def unitdm_pbc(u, l, occ=None):
-    h =tb(l,pbc=True)
-    for c in range(l//2):
-        h[2*c,2*c+1] += u[0]
-        h[2*c+1,2*c] += u[0]
-        h[2*c+1,2*c+1] += u[1]
-
-    e,v = np.linalg.eigh(h)
-    dm = np.zeros([l,l])
-    if occ is None:
-        occ = range(l//2)
-    for i in occ:
-        dm += np.outer(v[:,i], v[:,i])
-    return dm[:2,:2]
-    
-def gap4(u):
-    h =tb(4)
-    h[0,1] += u[0]
-    h[1,0] += u[0]
-    h[1,1] += u[1]
-    h[2,3] += u[0]
-    h[3,2] += u[0]
-    h[2,2] += u[1]
-    
-    e,v = np.linalg.eigh(h)
-    return e[2]-e[1]
-
 def gap_pbc(u, L):
+    """
+    half-filled gap, assuming pbc tb, chain L, and correlation potential u
+    """
     h =tb(L,pbc=True)
     for c in range(L//2):
         h[2*c,2*c+1] += u[0]
@@ -154,20 +85,30 @@ def get_embedding_h(u,L,occ=None):
     basis[2:,2:]=v.T
     return np.dot(basis.T, np.dot(h_pbc, basis))
     
-def fit(rho,L,occ=None):
+def fit(rho,L,occ=None,method="bfgs"):
+    """
+    global fit
+    """
     def error(u,occ=occ):
         dm = dm_pbc(u, L, occ)
         val = np.linalg.norm(rho-dm[:2,:2])
         return val
     u0 = np.array([0.1, 0.3])
-    res = scipy.optimize.minimize(error, u0, method='bfgs', options={"disp":True, "gtol":1.e-6})
-    return res.x, res.fun#[0], res[1]
+    if method == "bfgs":
+        res = scipy.optimize.minimize(error, u0, method='bfgs', options={"disp":True, "gtol":1.e-6})
+        return res.x, res.fun#[0], res[1]
+    elif method =="brute":
 
-    #res = scipy.optimize.brute(error, (slice(-3, 3, 0.05), slice(-4, 4, 0.25)),
-    #                           full_output=True, finish=scipy.optimize.fmin_bfgs)
-    #return res[0], res[1]
+        res = scipy.optimize.brute(error, (slice(-3, 3, 0.05), slice(-4, 4, 0.25)),
+                                   full_output=True, finish=scipy.optimize.fmin_bfgs)
+        return res[0], res[1]
+    else:
+        raise NotImplementedError
 
-def fit_local(rho,L,occ=None):
+def fit_local(rho,L,occ=None,method="bfgs"):
+    """
+    local u fit
+    """
     def error(u,occ=occ):
         h_emb=get_embedding_h(u,L,occ)
         dm=np.zeros_like(h_emb)
@@ -179,173 +120,244 @@ def fit_local(rho,L,occ=None):
         val = np.linalg.norm(rho-dm[:2,:2])
         return val
     u0 = [0, 0]
-    res = scipy.optimize.minimize(error, u0, method='BFGS', options={"disp":True, "gtol":1.e-6})
-    return res.x, res.fun#[0], res[1]
+    if method == "bfgs":
+        res = scipy.optimize.minimize(error, u0, method='BFGS', options={"disp":True, "gtol":1.e-6})
+        return res.x, res.fun#[0], res[1]
+    elif method == "brute":
 
-    #res = scipy.optimize.brute(error, (slice(-3, 3, 0.05), slice(-4, 4, 0.25)),
-    #                           full_output=True, finish=scipy.optimize.fmin_bfgs)
-    #return res[0], res[1]
+        res = scipy.optimize.brute(error, (slice(-3, 3, 0.05), slice(-4, 4, 0.25)),
+                                   full_output=True, finish=scipy.optimize.fmin_bfgs)
+        return res[0], res[1]
+    else:
+        raise NotImplementedError
 
 def gap_local(u,L):
     hemb=get_embedding_h(u,L)
     e,v=np.linalg.eigh(hemb)
     return e[2]-e[1]
-    
-def test_embedding():
-    # this script demonstrates that embedding non-aufbau leads to an excited state
-    # configuration of the local problem
-    L = 6
-    h_pbc=tb(L, pbc=True)
-    occ=[0,1,3]
-    print(dm_pbc([0,0],L,occ))
-    h_embed=get_embedding_h([0,0], L,occ)
-    print(h_embed)
-    e,v=np.linalg.eigh(h_embed)
 
-    print("------------")
-    print(np.outer(v[:,0],v[:,0])+np.outer(v[:,1],v[:,1]))
-    print(np.outer(v[:,0],v[:,0])+np.outer(v[:,2],v[:,2]))
-    print(np.outer(v[:,0],v[:,0])+np.outer(v[:,3],v[:,2]))
-    
-
-def test_equality():
-    col = []
-    gap_col = []
-    u0_col = []
-    u1_col = []
-
-    # check local vs global
-    n = 0.05
-    angle = 0.3*np.pi
-    rho0 = gen_rho(n, np.pi*angle)
-    
+def generate_data(occ,occ_loc):
+    import csv
     l = 6
-    for n in np.linspace(0.0, 1.0, 31):
-        col_tmp = []
-        gap_tmp = []
-        u0_tmp = []
-        u1_tmp = []
-        for angle in np.linspace(0, np.pi, 31):
-            rho0 = gen_rho(n, np.pi*angle)
+    occstr = "".join(str(o) for o in occ)
+    occlocstr = "".join(str(o) for o in occ_loc)
+    with open("data"+occstr+occlocstr, "w", newline='') as datafile:
+        writer = csv.writer(datafile)
+        writer.writerow(["n", "angle", "fun", "fun_local", "gap", "gap_loc", "small_gap", "small_gap_loc", "large_gap", "large_gap_loc", "big_diff"])
+        for n in np.linspace(0.0, 1.0, 31):
+            for angle in np.linspace(0, np.pi, 31):
+                rho0 = gen_rho(n, np.pi*angle)
+                ufit, fun = fit(rho0,l,occ)
+                ufit_local, fun_local = fit_local(rho0,l,occ_loc)
+                gap = gap_pbc(ufit,l)
+                gap_loc = gap_local(ufit,l)
+                small_gap = int(abs(gap)<1.e-3)
+                small_gap_loc = int(abs(gap_loc)<1.e-3)
+                large_gap = int(abs(gap)>5)
+                large_gap_loc = int(abs(gap_loc)>5)
+                big_diff = int(abs(fun - fun_local)>1.e-4)
 
-            print(np.linalg.eigvalsh(rho0))
-            ufit, fun = fit(rho0,l)
-            ufit_local, fun_local = fit_local(rho0,l)
-            print("global, local", fun,fun_local)
-            print(ufit)
-            print(ufit_local)
-            gap = gap_pbc(ufit,l)
-            gap_loc = gap_local(ufit,l)
-            print("gap\n",gap_pbc(ufit,l), gap_loc)
+                writer.writerow([n, angle, fun, fun_local, gap, gap_loc, small_gap, small_gap_loc, large_gap, large_gap_loc, big_diff])
             
-            dm_p = dm_pbc(ufit, l)
-            print(dm_p)
-
-            h_emb=get_embedding_h(ufit_local,l)
-            dm_l=np.zeros_like(h_emb)
-            e,v=np.linalg.eigh(h_emb)
-            dm_l = np.outer(v[:,0],v[:,0])+np.outer(v[:,1],v[:,1])
-            print(dm_l)
             
-            if(gap<1.e-3):
-                print("SMALL GAP===================")
-            if (abs(fun-fun_local)>1.e-5):
-                print("WARNING********************")
             
-            #print("fit error", fun)
-            print("fitted\n", unitdm_pbc(ufit,l))
-            print("target\n", rho0)
+#             print("global, local", fun,fun_local)
+#             print(ufit)
+#             print(ufit_local)
+#             print("gap\n",gap_pbc(ufit,l), gap_loc)
+            
+#             dm_p = dm_pbc(ufit, l)
+#             print(dm_p)
 
-            col_tmp.append(fun)
-            gap_tmp.append(gap_pbc(ufit,l))
-            u0_tmp.append(ufit[0])
-            u1_tmp.append(ufit[1])
+#             h_emb=get_embedding_h(ufit_local,l)
+#             dm_l=np.zeros_like(h_emb)
+#             e,v=np.linalg.eigh(h_emb)
+#             dm_l = np.outer(v[:,0],v[:,0])+np.outer(v[:,1],v[:,1])
+#             print(dm_l)
+            
+#             if(gap<1.e-3):
+#                 print("SMALL GAP===================")
+#             if (abs(fun-fun_local)>1.e-5):
+#                 print("WARNING********************")
+            
+#             #print("fit error", fun)
+#             print("fitted\n", unitdm_pbc(ufit,l))
+#             print("target\n", rho0)
 
-        col.append(col_tmp)
-        gap_col.append(gap_tmp)
-        u0_col.append(u0_tmp)
-        u1_col.append(u1_tmp)
+#             col_tmp.append(fun)
+#             gap_tmp.append(gap_pbc(ufit,l))
+#             u0_tmp.append(ufit[0])
+#             u1_tmp.append(ufit[1])
+
+#         col.append(col_tmp)
+#         gap_col.append(gap_tmp)
+#         u0_col.append(u0_tmp)
+#         u1_col.append(u1_tmp)
+
+
+
+            
+
+
+# def test_embedding():
+#     # this script demonstrates that embedding non-aufbau leads to an excited state
+#     # configuration of the local problem
+#     L = 6
+#     h_pbc=tb(L, pbc=True)
+#     occ=[0,1,3]
+#     print(dm_pbc([0,0],L,occ))
+#     h_embed=get_embedding_h([0,0], L,occ)
+#     print(h_embed)
+#     e,v=np.linalg.eigh(h_embed)
+
+#     print("------------")
+#     print(np.outer(v[:,0],v[:,0])+np.outer(v[:,1],v[:,1]))
+#     print(np.outer(v[:,0],v[:,0])+np.outer(v[:,2],v[:,2]))
+#     print(np.outer(v[:,0],v[:,0])+np.outer(v[:,3],v[:,2]))
+
+
+            
+    
+# def test_equality():
+#     col = []
+#     gap_col = []
+#     u0_col = []
+#     u1_col = []
+
+#     # check local vs global
+#     n = 0.05
+#     angle = 0.3*np.pi
+#     rho0 = gen_rho(n, np.pi*angle)
+    
+#     l = 6
+#     for n in np.linspace(0.0, 1.0, 31):
+#         col_tmp = []
+#         gap_tmp = []
+#         u0_tmp = []
+#         u1_tmp = []
+#         for angle in np.linspace(0, np.pi, 31):
+#             rho0 = gen_rho(n, np.pi*angle)
+
+#             print(np.linalg.eigvalsh(rho0))
+#             ufit, fun = fit(rho0,l)
+#             ufit_local, fun_local = fit_local(rho0,l)
+#             print("global, local", fun,fun_local)
+#             print(ufit)
+#             print(ufit_local)
+#             gap = gap_pbc(ufit,l)
+#             gap_loc = gap_local(ufit,l)
+#             print("gap\n",gap_pbc(ufit,l), gap_loc)
+            
+#             dm_p = dm_pbc(ufit, l)
+#             print(dm_p)
+
+#             h_emb=get_embedding_h(ufit_local,l)
+#             dm_l=np.zeros_like(h_emb)
+#             e,v=np.linalg.eigh(h_emb)
+#             dm_l = np.outer(v[:,0],v[:,0])+np.outer(v[:,1],v[:,1])
+#             print(dm_l)
+            
+#             if(gap<1.e-3):
+#                 print("SMALL GAP===================")
+#             if (abs(fun-fun_local)>1.e-5):
+#                 print("WARNING********************")
+            
+#             #print("fit error", fun)
+#             print("fitted\n", unitdm_pbc(ufit,l))
+#             print("target\n", rho0)
+
+#             col_tmp.append(fun)
+#             gap_tmp.append(gap_pbc(ufit,l))
+#             u0_tmp.append(ufit[0])
+#             u1_tmp.append(ufit[1])
+
+#         col.append(col_tmp)
+#         gap_col.append(gap_tmp)
+#         u0_col.append(u0_tmp)
+#         u1_col.append(u1_tmp)
 
 
     
-def test():
-    col = []
-    gap_col = []
-    u0_col = []
-    u1_col = []
+# def test():
+#     col = []
+#     gap_col = []
+#     u0_col = []
+#     u1_col = []
 
-#    ##### This code is to plot the surface of u for a given rho0 ########
-#    n = 0.05
-#    angle = 0.3*np.pi
-#    rho0 = gen_rho(n, np.pi*angle)
-#    print(rho0)
-#    print("rho0 eigs", np.linalg.eigvalsh(rho0))
-#    ufit, fun = fit(rho0, unitdm4)
-#    print(gap4(ufit))
-#    print(ufit, fun)
-#
-#    print("dm eigs", np.linalg.eigvalsh(unitdm4(ufit)))
-#    
-#    for u0 in np.linspace(-3.,3.,30):
-#        col_tmp = []
-#        u0_tmp = []
-#        u1_tmp = []
-#
-#        for u1 in np.linspace (-3., 3., 30):
-#            val = np.linalg.norm(rho0-unitdm4([u0,u1]))
-#            print(rho0)
-#            print(unitdm4([u0,u1]))
-#            print("dm eigs", np.linalg.eigvalsh(unitdm4([u0,u1])))
-#            print("gap", gap4([u0,u1]))
-#            print("params, val", u0, u1, val)
-#            col_tmp.append(val)
-#            u0_tmp.append(u0)
-#            u1_tmp.append(u1)
-#
-#        col.append(col_tmp)
-#        u0_col.append(u0_tmp)
-#        u1_col.append(u1_tmp)
+# #    ##### This code is to plot the surface of u for a given rho0 ########
+# #    n = 0.05
+# #    angle = 0.3*np.pi
+# #    rho0 = gen_rho(n, np.pi*angle)
+# #    print(rho0)
+# #    print("rho0 eigs", np.linalg.eigvalsh(rho0))
+# #    ufit, fun = fit(rho0, unitdm4)
+# #    print(gap4(ufit))
+# #    print(ufit, fun)
+# #
+# #    print("dm eigs", np.linalg.eigvalsh(unitdm4(ufit)))
+# #    
+# #    for u0 in np.linspace(-3.,3.,30):
+# #        col_tmp = []
+# #        u0_tmp = []
+# #        u1_tmp = []
+# #
+# #        for u1 in np.linspace (-3., 3., 30):
+# #            val = np.linalg.norm(rho0-unitdm4([u0,u1]))
+# #            print(rho0)
+# #            print(unitdm4([u0,u1]))
+# #            print("dm eigs", np.linalg.eigvalsh(unitdm4([u0,u1])))
+# #            print("gap", gap4([u0,u1]))
+# #            print("params, val", u0, u1, val)
+# #            col_tmp.append(val)
+# #            u0_tmp.append(u0)
+# #            u1_tmp.append(u1)
+# #
+# #        col.append(col_tmp)
+# #        u0_col.append(u0_tmp)
+# #        u1_col.append(u1_tmp)
 
-    #### This code tests the 6 site pbc model    
-    l = 6
-    for n in np.linspace(0.0, 1.0, 31):
-        col_tmp = []
-        gap_tmp = []
-        u0_tmp = []
-        u1_tmp = []
-        for angle in np.linspace(0, np.pi, 31):
-            rho0 = gen_rho(n, np.pi*angle)
-            print("target\n", rho0)
-            print(np.linalg.eigvalsh(rho0))
-            ufit, fun = fit(rho0,l)
-            print("fit error", fun)
-            print("fitted\n", unitdm_pbc(ufit,l))
-            print("gap\n",gap_pbc(ufit,l))
-            col_tmp.append(fun)
-            gap_tmp.append(gap_pbc(ufit,l))
-            u0_tmp.append(ufit[0])
-            u1_tmp.append(ufit[1])
+#     #### This code tests the 6 site pbc model    
+#     l = 6
+#     for n in np.linspace(0.0, 1.0, 31):
+#         col_tmp = []
+#         gap_tmp = []
+#         u0_tmp = []
+#         u1_tmp = []
+#         for angle in np.linspace(0, np.pi, 31):
+#             rho0 = gen_rho(n, np.pi*angle)
+#             print("target\n", rho0)
+#             print(np.linalg.eigvalsh(rho0))
+#             ufit, fun = fit(rho0,l)
+#             print("fit error", fun)
+#             print("fitted\n", unitdm_pbc(ufit,l))
+#             print("gap\n",gap_pbc(ufit,l))
+#             col_tmp.append(fun)
+#             gap_tmp.append(gap_pbc(ufit,l))
+#             u0_tmp.append(ufit[0])
+#             u1_tmp.append(ufit[1])
 
-        col.append(col_tmp)
-        gap_col.append(gap_tmp)
-        u0_col.append(u0_tmp)
-        u1_col.append(u1_tmp)
+#         col.append(col_tmp)
+#         gap_col.append(gap_tmp)
+#         u0_col.append(u0_tmp)
+#         u1_col.append(u1_tmp)
 
-    col = np.array(col)
-    np.save("col.npy", col)
-    gap_col = np.array(gap_col)
-    np.save("gap_col.npy", gap_col)
-    u0_col = np.array(u0_col)
-    np.save("u0_col.npy", u0_col)
-    u1_col = np.array(u1_col)
-    np.save("u1_col.npy", u1_col)
-    print (col)
+#     col = np.array(col)
+#     np.save("col.npy", col)
+#     gap_col = np.array(gap_col)
+#     np.save("gap_col.npy", gap_col)
+#     u0_col = np.array(u0_col)
+#     np.save("u0_col.npy", u0_col)
+#     u1_col = np.array(u1_col)
+#     np.save("u1_col.npy", u1_col)
+#     print (col)
 
-if __name__ == '__main__':
-    test_gen_rho()
-    test_tb()
-    exit()
+# if __name__ == '__main__':
+#     test_gen_rho()
+#     test_tb()
+#     exit()
 
-    print (gen_rho(n, theta))
-    #test()
-    test_equality()
+#     print (gen_rho(n, theta))
+#     #test()
+#     test_equality()
+if __name__=='__main__':
+    generate_data([0,1,2],[0,1])
