ca437a528c05705a20be691931bff7cfae5277a6
Garnet Chan <garnet@dhcp-21-150.caltech.edu>
2021-09-05 18:12:20 -0700

    checkin

--- vrepr.py
+++ vrepr.py
@@ -3,15 +3,22 @@ import numpy as np
 import scipy.optimize
 
 def genrho(n, theta):
+    """
+    2x2 density matrix for two sites
+    """
+    assert 0<=n<=1
     occ = np.diag([n, 1.-n])
     u = np.array([[np.cos(theta), np.sin(theta)],
                   [-np.sin(theta), np.cos(theta)]])
     return np.dot(u.T, np.dot(occ, u))
 
-def tb(n, pbc=False):
-    h = np.zeros([n,n])
-    for i in range(n):
-        for j in range(n):
+def tb(L, pbc=False):
+    """
+    tight-binding Hamiltonian for lattice of length L
+    """
+    h = np.zeros([L,L])
+    for i in range(L):
+        for j in range(L):
             if abs(i-j)==1: h[i,j]=1.
     #pbc=True
     if pbc:
@@ -24,25 +31,42 @@ def unitdm4(u, full=False):
     h[0,1] += u[0]
     h[1,0] += u[0]
     h[1,1] += u[1]
-    #h[2:,2:] = h[0:2,0:2]
-    #h[2,3] += u[0]
-    #h[3,2] += u[0]
-    #h[2,2] += u[1]
+    h[2,3] += u[0]
+    h[3,2] += u[0]
+    h[2,2] += u[1]
     
     e,v = np.linalg.eigh(h)
 
-    #print (h)
-    #print (e)
-    #print(v)
-    #dm = np.outer(v[:,0], v[:,0]) + np.outer(v[:,1], v[:,1])
     dm = np.outer(v[:,0], v[:,0]) + np.outer(v[:,1], v[:,1])
-    #print (dm)
     if full:
         return dm
     else:
         return dm[:2,:2]
 
-def unitdm_pbc(u, l):
+
+def add_u_pbc(h, u):
+    L=h.shape[0]
+    hu = h.copy()
+    for c in range(L//2):
+        hu[2*c,2*c+1] += u[0]
+        hu[2*c+1,2*c] += u[0]
+        hu[2*c+1,2*c+1] += u[1]
+    return hu
+    
+def dm_pbc(u, L, occ=None):
+    h=tb(L,pbc=True)
+    h = add_u_pbc(h, u)
+
+    e,v = np.linalg.eigh(h)
+    dm = np.zeros([L,L])
+    if occ is None:
+        occ = range(L//2)
+    for i in occ:
+        dm += np.outer(v[:,i], v[:,i])
+    return dm
+
+    
+def unitdm_pbc(u, l, occ=None):
     h =tb(l,pbc=True)
     for c in range(l//2):
         h[2*c,2*c+1] += u[0]
@@ -51,7 +75,9 @@ def unitdm_pbc(u, l):
 
     e,v = np.linalg.eigh(h)
     dm = np.zeros([l,l])
-    for i in range(l//2):
+    if occ is None:
+        occ = range(l//2)
+    for i in occ:
         dm += np.outer(v[:,i], v[:,i])
     return dm[:2,:2]
     
@@ -67,30 +93,144 @@ def gap4(u):
     e,v = np.linalg.eigh(h)
     return e[2]-e[1]
 
-def gap_pbc(u, l):
-    h =tb(l,pbc=True)
-    for c in range(l//2):
+def gap_pbc(u, L):
+    h =tb(L,pbc=True)
+    for c in range(L//2):
         h[2*c,2*c+1] += u[0]
         h[2*c+1,2*c] += u[0]
         h[2*c+1,2*c+1] += u[1]
 
     e,v = np.linalg.eigh(h)
-    print(e)
-    return e[l//2]-e[l//2-1]
+    return e[L//2]-e[L//2-1]
+
+def get_embedding_h(u,L,occ=None):
+    """
+    embedding Hamiltonian for first two sites
+    """
+    h_pbc=tb(L,pbc=True)
+    h_pbc = add_u_pbc(h_pbc, u)
+    dm=dm_pbc(u,L,occ)
+    u,s,v=np.linalg.svd(dm[:2,2:], full_matrices=False)
+
+    basis = np.zeros([L, 4])
+    basis[:2,:2]=np.eye(2)
+    basis[2:,2:]=v.T
+    return np.dot(basis.T, np.dot(h_pbc, basis))
     
-def fit(rho, dmfn=None):
-    def error(u):
-        val = np.linalg.norm(rho-dmfn(u))
+def fit(rho,L,occ=None):
+    def error(u,occ=occ):
+        dm = dm_pbc(u, L, occ)
+        val = np.linalg.norm(rho-dm[:2,:2])
         return val
     u0 = np.array([0.1, 0.3])
-    res = scipy.optimize.minimize(error, u0, method='BFGS', options={"disp":True, "gtol":1.e-4})
-    #res = scipy.optimize.minimize(error, u0, method='L-BFGS-B', options={"disp":False, "gtol":1.e-4})
-    #return res.x, res.fun
-    #res = scipy.optimize.brute(error, (slice(-1, 1, 0.05), slice(-4, 4, 0.25)),
-    #                           full_output=True, finish=scipy.optimize.fmin)
+    res = scipy.optimize.minimize(error, u0, method='bfgs', options={"disp":True, "gtol":1.e-6})
     return res.x, res.fun#[0], res[1]
+
+    #res = scipy.optimize.brute(error, (slice(-3, 3, 0.05), slice(-4, 4, 0.25)),
+    #                           full_output=True, finish=scipy.optimize.fmin_bfgs)
     #return res[0], res[1]
 
+def fit_local(rho,L,occ=None):
+    def error(u,occ=occ):
+        h_emb=get_embedding_h(u,L,occ)
+        dm=np.zeros_like(h_emb)
+        e,v=np.linalg.eigh(h_emb)
+        if occ is None:
+            occ = [0,1]
+        for i in occ:
+            dm+=np.outer(v[:,i],v[:,i])
+        val = np.linalg.norm(rho-dm[:2,:2])
+        return val
+    u0 = [0, 0]
+    res = scipy.optimize.minimize(error, u0, method='BFGS', options={"disp":True, "gtol":1.e-6})
+    return res.x, res.fun#[0], res[1]
+
+    #res = scipy.optimize.brute(error, (slice(-3, 3, 0.05), slice(-4, 4, 0.25)),
+    #                           full_output=True, finish=scipy.optimize.fmin_bfgs)
+    #return res[0], res[1]
+
+def gap_local(u,L):
+    hemb=get_embedding_h(u,L)
+    e,v=np.linalg.eigh(hemb)
+    return e[2]-e[1]
+    
+def test_embedding():
+    # this script demonstrates that embedding non-aufbau leads to an excited state
+    # configuration of the local problem
+    L = 6
+    h_pbc=tb(L, pbc=True)
+    occ=[0,1,3]
+    print(dm_pbc([0,0],L,occ))
+    h_embed=get_embedding_h([0,0], L,occ)
+    print(h_embed)
+    e,v=np.linalg.eigh(h_embed)
+
+    print("------------")
+    print(np.outer(v[:,0],v[:,0])+np.outer(v[:,1],v[:,1]))
+    print(np.outer(v[:,0],v[:,0])+np.outer(v[:,2],v[:,2]))
+    print(np.outer(v[:,0],v[:,0])+np.outer(v[:,3],v[:,2]))
+    
+
+def test_equality():
+    col = []
+    gap_col = []
+    u0_col = []
+    u1_col = []
+
+    # check local vs global
+    n = 0.05
+    angle = 0.3*np.pi
+    rho0 = genrho(n, np.pi*angle)
+    
+    l = 6
+    for n in np.linspace(0.0, 1.0, 31):
+        col_tmp = []
+        gap_tmp = []
+        u0_tmp = []
+        u1_tmp = []
+        for angle in np.linspace(0, np.pi, 31):
+            rho0 = genrho(n, np.pi*angle)
+
+            print(np.linalg.eigvalsh(rho0))
+            ufit, fun = fit(rho0,l)
+            ufit_local, fun_local = fit_local(rho0,l)
+            print("global, local", fun,fun_local)
+            print(ufit)
+            print(ufit_local)
+            gap = gap_pbc(ufit,l)
+            gap_loc = gap_local(ufit,l)
+            print("gap\n",gap_pbc(ufit,l), gap_loc)
+            
+            dm_p = dm_pbc(ufit, l)
+            print(dm_p)
+
+            h_emb=get_embedding_h(ufit_local,l)
+            dm_l=np.zeros_like(h_emb)
+            e,v=np.linalg.eigh(h_emb)
+            dm_l = np.outer(v[:,0],v[:,0])+np.outer(v[:,1],v[:,1])
+            print(dm_l)
+            
+            if(gap<1.e-3):
+                print("SMALL GAP===================")
+            if (abs(fun-fun_local)>1.e-5):
+                print("WARNING********************")
+            
+            #print("fit error", fun)
+            print("fitted\n", unitdm_pbc(ufit,l))
+            print("target\n", rho0)
+
+            col_tmp.append(fun)
+            gap_tmp.append(gap_pbc(ufit,l))
+            u0_tmp.append(ufit[0])
+            u1_tmp.append(ufit[1])
+
+        col.append(col_tmp)
+        gap_col.append(gap_tmp)
+        u0_col.append(u0_tmp)
+        u1_col.append(u1_tmp)
+
+
+    
 def test():
     col = []
     gap_col = []
@@ -98,6 +238,8 @@ def test():
     u1_col = []
 
 
+
+    
     ##### This code is to plot the surface of u for a given rho0 ########
     n = 0.05
     angle = 0.3*np.pi
